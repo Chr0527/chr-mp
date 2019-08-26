55ccb81a1b21c7e8c04942379866758676b0b31b
Chr0527 <1015670600@qq.com>
2019-08-26 15:05:41 +0800

    Initial commit

---
+++ src/main/java/com/chryl/ChrMpApplication.java
@@ -0,0 +1,13 @@
+package com.chryl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChrMpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChrMpApplication.class, args);
+	}
+
+}
